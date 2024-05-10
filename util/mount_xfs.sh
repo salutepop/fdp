be7c51c63a41b921b6173e89d743c6397bdb013c
Changmin Jeon <salutepop@gmail.com>
2024-05-11 00:53:56 +0900

    add mount_xfs scripts

---
+++ util/mount_xfs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ $# -lt 2 ]]; then
+  echo "Illegal number of parameters"
+  echo "(ex) ./mount_xfs.sh nvme0n1 /home/cm/mounted_dir"
+  exit
+fi
+
+DEV=$1
+MOUNT=$2
+
+DEVICE="/dev/"$DEV
+
+# 0. mkfs & mount
+sudo fuser -ck $DEVICE
+sudo umount $DEVICE
+sudo fio --name=trim --filename=$DEVICE --rw=trim --bs=3G
+sudo mkfs.xfs -f $DEVICE
+rm -r $MOUNT
+mkdir $MOUNT
+sudo mount $DEVICE $MOUNT
+sudo chown -R cm $MOUNT
+
+MOUNTED=`df -h | grep $DEV | wc -l`
+if [[ $MOUNTED -ne 1 ]]; then
+  echo "not mounted path : $MOUNTED"
+  exit
+else
+  df -h | grep $DEV
+fi
