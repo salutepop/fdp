1f5d09a07cd483f5f511841bb6881ea89fd8aee6
Changmin Jeon <salutepop@gmail.com>
2024-05-11 01:02:08 +0900

    add fio scripts (rawdevice)

---
+++ fio/test_rawdevice.sh
@@ -0,0 +1,66 @@
+DEVICE=$1
+RUNTIME=30
+
+RW='read'
+CHUNK='4k'
+sudo fio --name=psync_$RW'_'$CHUNK --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=psync --rw=$RW --time_based --runtime=$RUNTIME --direct=1 --output-format=normal,terse
+sudo fio --name=uring_$RW'_'$CHUNK'_qd1' --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=io_uring --rw=$RW --time_based --runtime=$RUNTIME --iodepth=1 --output-format=normal,terse
+sudo fio --name=uring_$RW'_'$CHUNK'_qd32' --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=io_uring --rw=$RW --time_based --runtime=$RUNTIME --iodepth=32 --output-format=normal,terse
+sudo fio --name=uring_cmd_$RW'_'$CHUNK'_qd1' --filename=/dev/ng$DEVICE --bs=$CHUNK --ioengine=io_uring_cmd --rw=$RW --time_based --runtime=$RUNTIME --iodepth=1 --output-format=normal,terse
+sudo fio --name=uring_cmd_$RW'_'$CHUNK'_qd32' --filename=/dev/ng$DEVICE --bs=$CHUNK --ioengine=io_uring_cmd --rw=$RW --time_based --runtime=$RUNTIME --iodepth=32 --output-format=normal,terse
+
+RW='read'
+CHUNK='16k'
+sudo fio --name=psync_$RW'_'$CHUNK --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=psync --rw=$RW --time_based --runtime=$RUNTIME --direct=1 --output-format=normal,terse
+sudo fio --name=uring_$RW'_'$CHUNK'_qd1' --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=io_uring --rw=$RW --time_based --runtime=$RUNTIME --iodepth=1 --output-format=normal,terse
+sudo fio --name=uring_$RW'_'$CHUNK'_qd32' --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=io_uring --rw=$RW --time_based --runtime=$RUNTIME --iodepth=32 --output-format=normal,terse
+sudo fio --name=uring_cmd_$RW'_'$CHUNK'_qd1' --filename=/dev/ng$DEVICE --bs=$CHUNK --ioengine=io_uring_cmd --rw=$RW --time_based --runtime=$RUNTIME --iodepth=1 --output-format=normal,terse
+sudo fio --name=uring_cmd_$RW'_'$CHUNK'_qd32' --filename=/dev/ng$DEVICE --bs=$CHUNK --ioengine=io_uring_cmd --rw=$RW --time_based --runtime=$RUNTIME --iodepth=32 --output-format=normal,terse
+
+RW='read'
+CHUNK='64k'
+sudo fio --name=psync_$RW'_'$CHUNK --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=psync --rw=$RW --time_based --runtime=$RUNTIME --direct=1 --output-format=normal,terse
+sudo fio --name=uring_$RW'_'$CHUNK'_qd1' --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=io_uring --rw=$RW --time_based --runtime=$RUNTIME --iodepth=1 --output-format=normal,terse
+sudo fio --name=uring_$RW'_'$CHUNK'_qd32' --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=io_uring --rw=$RW --time_based --runtime=$RUNTIME --iodepth=32 --output-format=normal,terse
+sudo fio --name=uring_cmd_$RW'_'$CHUNK'_qd1' --filename=/dev/ng$DEVICE --bs=$CHUNK --ioengine=io_uring_cmd --rw=$RW --time_based --runtime=$RUNTIME --iodepth=1 --output-format=normal,terse
+sudo fio --name=uring_cmd_$RW'_'$CHUNK'_qd32' --filename=/dev/ng$DEVICE --bs=$CHUNK --ioengine=io_uring_cmd --rw=$RW --time_based --runtime=$RUNTIME --iodepth=32 --output-format=normal,terse
+
+RW='read'
+CHUNK='256k'
+sudo fio --name=psync_$RW'_'$CHUNK --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=psync --rw=$RW --time_based --runtime=$RUNTIME --direct=1 --output-format=normal,terse
+sudo fio --name=uring_$RW'_'$CHUNK'_qd1' --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=io_uring --rw=$RW --time_based --runtime=$RUNTIME --iodepth=1 --output-format=normal,terse
+sudo fio --name=uring_$RW'_'$CHUNK'_qd32' --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=io_uring --rw=$RW --time_based --runtime=$RUNTIME --iodepth=32 --output-format=normal,terse
+sudo fio --name=uring_cmd_$RW'_'$CHUNK'_qd1' --filename=/dev/ng$DEVICE --bs=$CHUNK --ioengine=io_uring_cmd --rw=$RW --time_based --runtime=$RUNTIME --iodepth=1 --output-format=normal,terse
+sudo fio --name=uring_cmd_$RW'_'$CHUNK'_qd32' --filename=/dev/ng$DEVICE --bs=$CHUNK --ioengine=io_uring_cmd --rw=$RW --time_based --runtime=$RUNTIME --iodepth=32 --output-format=normal,terse
+
+RW='write'
+CHUNK='4k'
+sudo fio --name=psync_$RW'_'$CHUNK --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=psync --rw=$RW --time_based --runtime=$RUNTIME --direct=1 --output-format=normal,terse
+sudo fio --name=uring_$RW'_'$CHUNK'_qd1' --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=io_uring --rw=$RW --time_based --runtime=$RUNTIME --iodepth=1 --output-format=normal,terse
+sudo fio --name=uring_$RW'_'$CHUNK'_qd32' --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=io_uring --rw=$RW --time_based --runtime=$RUNTIME --iodepth=32 --output-format=normal,terse
+sudo fio --name=uring_cmd_$RW'_'$CHUNK'_qd1' --filename=/dev/ng$DEVICE --bs=$CHUNK --ioengine=io_uring_cmd --rw=$RW --time_based --runtime=$RUNTIME --iodepth=1 --output-format=normal,terse
+sudo fio --name=uring_cmd_$RW'_'$CHUNK'_qd32' --filename=/dev/ng$DEVICE --bs=$CHUNK --ioengine=io_uring_cmd --rw=$RW --time_based --runtime=$RUNTIME --iodepth=32 --output-format=normal,terse
+
+RW='write'
+CHUNK='16k'
+sudo fio --name=psync_$RW'_'$CHUNK --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=psync --rw=$RW --time_based --runtime=$RUNTIME --direct=1 --output-format=normal,terse
+sudo fio --name=uring_$RW'_'$CHUNK'_qd1' --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=io_uring --rw=$RW --time_based --runtime=$RUNTIME --iodepth=1 --output-format=normal,terse
+sudo fio --name=uring_$RW'_'$CHUNK'_qd32' --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=io_uring --rw=$RW --time_based --runtime=$RUNTIME --iodepth=32 --output-format=normal,terse
+sudo fio --name=uring_cmd_$RW'_'$CHUNK'_qd1' --filename=/dev/ng$DEVICE --bs=$CHUNK --ioengine=io_uring_cmd --rw=$RW --time_based --runtime=$RUNTIME --iodepth=1 --output-format=normal,terse
+sudo fio --name=uring_cmd_$RW'_'$CHUNK'_qd32' --filename=/dev/ng$DEVICE --bs=$CHUNK --ioengine=io_uring_cmd --rw=$RW --time_based --runtime=$RUNTIME --iodepth=32 --output-format=normal,terse
+
+RW='write'
+CHUNK='64k'
+sudo fio --name=psync_$RW'_'$CHUNK --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=psync --rw=$RW --time_based --runtime=$RUNTIME --direct=1 --output-format=normal,terse
+sudo fio --name=uring_$RW'_'$CHUNK'_qd1' --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=io_uring --rw=$RW --time_based --runtime=$RUNTIME --iodepth=1 --output-format=normal,terse
+sudo fio --name=uring_$RW'_'$CHUNK'_qd32' --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=io_uring --rw=$RW --time_based --runtime=$RUNTIME --iodepth=32 --output-format=normal,terse
+sudo fio --name=uring_cmd_$RW'_'$CHUNK'_qd1' --filename=/dev/ng$DEVICE --bs=$CHUNK --ioengine=io_uring_cmd --rw=$RW --time_based --runtime=$RUNTIME --iodepth=1 --output-format=normal,terse
+sudo fio --name=uring_cmd_$RW'_'$CHUNK'_qd32' --filename=/dev/ng$DEVICE --bs=$CHUNK --ioengine=io_uring_cmd --rw=$RW --time_based --runtime=$RUNTIME --iodepth=32 --output-format=normal,terse
+
+RW='write'
+CHUNK='256k'
+sudo fio --name=psync_$RW'_'$CHUNK --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=psync --rw=$RW --time_based --runtime=$RUNTIME --direct=1 --output-format=normal,terse
+sudo fio --name=uring_$RW'_'$CHUNK'_qd1' --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=io_uring --rw=$RW --time_based --runtime=$RUNTIME --iodepth=1 --output-format=normal,terse
+sudo fio --name=uring_$RW'_'$CHUNK'_qd32' --filename=/dev/nvme$DEVICE --bs=$CHUNK --ioengine=io_uring --rw=$RW --time_based --runtime=$RUNTIME --iodepth=32 --output-format=normal,terse
+sudo fio --name=uring_cmd_$RW'_'$CHUNK'_qd1' --filename=/dev/ng$DEVICE --bs=$CHUNK --ioengine=io_uring_cmd --rw=$RW --time_based --runtime=$RUNTIME --iodepth=1 --output-format=normal,terse
+sudo fio --name=uring_cmd_$RW'_'$CHUNK'_qd32' --filename=/dev/ng$DEVICE --bs=$CHUNK --ioengine=io_uring_cmd --rw=$RW --time_based --runtime=$RUNTIME --iodepth=32 --output-format=normal,terse
